bb053b3703c8fc3fec84968f1212f1c5b55410d8
pkosanin <pkosanin@griddynamics.com>
2021-01-11 16:56:03 +0100

    refactor starting point

--- employee/src/main/java/com/bamboo/employee/CommandLineAppRunner.java
+++ employee/src/main/java/com/bamboo/employee/CommandLineAppRunner.java
@@ -2,7 +2,6 @@ package com.bamboo.employee;
 
 import com.bamboo.employee.service.*;
 import com.bamboo.employee.service.command.CommandInvoker;
-import com.bamboo.employee.service.command.RemoveEmployeeCommand;
 import com.bamboo.employee.service.validationstrategy.Validator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,7 +10,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 
 @Component
@@ -21,12 +19,28 @@ public class CommandLineAppRunner implements CommandLineRunner {
             LoggerFactory.getLogger(CommandLineAppRunner.class);
 
     @Autowired
-    private EmployeeService service;
+    private EmployeeService employeeService;
+    @Autowired
+    private Validator validator;
+    @Autowired
+    private CommandInvoker commandInvoker;
 
     private void guideForSupportedCommands(final String command) {
         System.out.println(
                 "Seems like you passed unsupported command: " + command);
         System.out.println("Use help to see all supported commands");
+    }
+
+    private void runCommand(final String... args) {
+        try {
+            String[] inputArgs = Arrays.stream(args).skip(1).toArray(String[]::new);
+            Map<String, String> commandArgs = ArgumentParser.parseData(inputArgs);
+            Map<String, String> validatedCommandArgs = validator.validateAndRemoveRedundantArgs(args[0], commandArgs);
+            commandInvoker.executeCommand(args[0], validatedCommandArgs, employeeService);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Bad input");
+            System.exit(64);
+        }
     }
 
     @Override
@@ -35,27 +49,11 @@ public class CommandLineAppRunner implements CommandLineRunner {
                 "Application context started with command line arguments: "
                         + Arrays.toString(args));
 
-
-        Validator validator = new Validator();
-        CommandInvoker invoker = new CommandInvoker();
         if (SupportedCommands.isSupportedCommand(args[0])) {
-            try {
-                String[] namedArgs = Arrays.stream(args).skip(1).toArray(String[]::new);
-                Map<String, String> data = ArgumentParser.parseData(namedArgs);
-                validator.setStrategy(ValidationStrategyPicker.pickStrategy(args[0]));
-                data = validator.validateAndRemoveRedundantArgs(data);
-                invoker.setCommand(CommandPicker.pickCommand(args[0], service, data));
-                invoker.executeCommand();
-
-            } catch (IllegalArgumentException e) {
-                System.out.println("Bad input");
-                System.exit(1);
-            }
+            runCommand(args);
         } else {
             guideForSupportedCommands(args[0]);
         }
-
-
     }
 
 }
