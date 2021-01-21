674cbd5cb55f1926ad7006bd2f10e8ea73a8b8c9
aleksandarjocic <jocica@aeo.com>
2021-01-21 10:55:23 +0100

    Make application to be web application

--- employee/src/main/java/com/bamboo/employee/CommandLineAppRunner.java
+++ employee/src/main/java/com/bamboo/employee/CommandLineAppRunner.java
@@ -3,11 +3,14 @@ package com.bamboo.employee;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
 @Component
+@ConditionalOnProperty(value="spring.main.web-application-type", havingValue = "none")
 public class CommandLineAppRunner implements CommandLineRunner {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CommandLineAppRunner.class);
